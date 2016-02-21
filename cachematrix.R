db8dcf22abda70aeb86fdf3942b24214e370a81e
sacbhat <sacbhat@deloitte.com>
2016-02-21 13:47:02 +0530

    Added files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,6 @@
 
 # makeCacheMatrix is a function that returns a list of functions. 
-# Its puspose is to store a martix and a cached value of the inverse of the matrix. Contains the following functions:
+# Its purposepose is to store a martix and a cached value of the inverse of the matrix. Contains the following functions:
 # Args:
 #   x: A matrix (Optional)
 #
